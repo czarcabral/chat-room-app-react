5f3a37c9a2ec2ae7350130e9b827cf78a73da538
Gabriel Cabral <czar.cabral@gmail.com>
2021-01-16 15:25:30 -0800

    beginning of part 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,52 @@ import React, { Component } from "react";
 import './App.css';
 import { connect, sendMsg } from "./api";
 import Header from './components/Header/Header';
+import ChatHistory from "./components/ChatHistory";
 
-class App extends Component {
+interface AppProps {
+}
+interface AppState {
+  chatHistory: any;
+  mynum: number;
+}
+
+class App extends Component<AppProps, AppState> {
   constructor(props: any) {
     super(props);
-    connect();
+    this.state = {
+      chatHistory: [],
+      mynum: 1,
+    }
+    // this.send = this.send.bind(this);
   }
 
-  send() {
-    console.log("hello");
-    sendMsg("hello");
+  componentDidMount() {
+    console.log(this.state);
+    connect((msg: string) => {
+      console.log("New Message");
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg],
+        mynum: this.state.mynum + 1,
+      }))
+      console.log(this.state);
+    })
+  }
+
+  // send() {
+  //   console.log("hello", this.state.mynum);
+  //   sendMsg("hello" + this.state.mynum);
+  // }
+
+  send = () => {
+    console.log("hello", this.state.mynum);
+    sendMsg("hello" + this.state.mynum);
   }
 
   render() {
     return (
       <div className="App">
         <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
         <button onClick={this.send}>Hit</button>
       </div>
     );
--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,12 +1,15 @@
+// this is a 2 way connection to backend
 let socket = new WebSocket("ws://localhost:8080/ws");
 
-export var connect = () => {
+// this is to receive messages from backend
+export var connect = (cb: Function) => {
   console.log("Attempting Connection...");
   socket.onopen = () => {
     console.log("Successfully Connected");
   }
   socket.onmessage = msg => {
     console.log(msg);
+    cb(msg);
   }
   socket.onclose = event => {
     console.log("Socket Closed Connection: ", event);
@@ -16,6 +19,7 @@ export var connect = () => {
   }
 }
 
+// this is to send messages to backend
 let sendMsg = (msg: string) => {
   console.log("sending msg: ", msg);
   socket.send(msg);
